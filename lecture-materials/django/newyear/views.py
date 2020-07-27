532dbef598b7e365465889d8ae781eea8cc59a42
Jerald <jerald@Jeralds-MacBook-Pro.local>
2020-07-27 22:44:02 +0800

    Add lecture materials and initial Project 2

---
+++ lecture-materials/django/newyear/views.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    now = datetime.now()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
+    # return HttpResponse("Hello world")
